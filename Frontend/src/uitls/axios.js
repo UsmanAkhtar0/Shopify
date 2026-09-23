965b5580ac2dc79aa8ee75a36b423c982b66ea27
Usman Akhtar <ususmanakhtar@gmail.com>
2026-09-23 17:09:27 +0530

    change in axios.js

--- Frontend/src/uitls/axios.js
+++ Frontend/src/uitls/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://shopifybackend-pxpl.onrender.com/api",  // "http://localhost:8000/api", for Prod "https://shopifybackend-pxpl.onrender.com/api"
-    withCredentials: true    // allow cookies to be send
+    baseURL: "https://shopifybackend-pxpl.onrender.com/api"  // "http://localhost:8000/api", for Prod "https://shopifybackend-pxpl.onrender.com/api"
+    // withCredentials: true    // allow cookies to be send
 })
 
 export default instance; 
